e0e298a98bc117b5416039029276d9278b363db0
laurawieser <laura.wieser1@stud.fh-campuswien.ac.at>
2025-05-31 18:10:12 +0200

    Some fixes regarding tests

--- src/main/test/at/ac/fhcampuswien/fhmdb/ControllerSingletonTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/ControllerSingletonTest.java
@@ -1,9 +1,12 @@
+package at.ac.fhcampuswien.fhmdb;
+
 import at.ac.fhcampuswien.fhmdb.controllers.MainController;
 import at.ac.fhcampuswien.fhmdb.controllers.MovieListController;
 import at.ac.fhcampuswien.fhmdb.controllers.WatchlistController;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ControllerSingletonTest {
 
@@ -34,39 +37,5 @@ public class ControllerSingletonTest {
         assertSame(firstInstance, secondInstance);
     }
 }
-import at.ac.fhcampuswien.fhmdb.controllers.MainController;
-import at.ac.fhcampuswien.fhmdb.controllers.MovieListController;
-import at.ac.fhcampuswien.fhmdb.controllers.WatchlistController;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-public class ControllerSingletonTest {
-
-    @Test
-    public void testMainControllerIsSingleton() {
-        MainController firstInstance = new MainController();
-        MainController secondInstance = MainController.getInstance();
-
-        assertNotNull(secondInstance);
-        assertSame(firstInstance, secondInstance);
-    }
-
-    @Test
-    public void testMovieListControllerIsSingleton() {
-        MovieListController firstInstance = new MovieListController();
-        MovieListController secondInstance = MovieListController.getInstance();
-
-        assertNotNull(secondInstance);
-        assertSame(firstInstance, secondInstance);
-    }
-
-    @Test
-    public void testWatchlistControllerIsSingleton() {
-        WatchlistController firstInstance = new WatchlistController();
-        WatchlistController secondInstance = WatchlistController.getInstance();
-
-        assertNotNull(secondInstance);
-        assertSame(firstInstance, secondInstance);
-    }
-}
